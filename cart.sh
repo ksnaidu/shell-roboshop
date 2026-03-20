6f1ac18393023b58d7fdd386b219868a8a3a8de6
ksnaidu <ksnaidudba@gmail.com>
2026-03-20 12:17:59 +0530

    cart added

--- cart.sh
+++ cart.sh
@@ -15,7 +15,7 @@ mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 # check the cart has root priveleges or not
-if [ $userID -ne 0 ]
+if [ "$USERID" -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
     exit 1 #give other than 0 upto 127

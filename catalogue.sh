87ae53fc64353e6574d9155b779f6c2de6a7beda
ksnaidu <ksnaidudba@gmail.com>
2026-03-28 10:15:50 +0530

    catalogue.sh added

--- catalogue.sh
+++ catalogue.sh
@@ -42,6 +42,7 @@ VALIDATE $? "Enabling nodejs:20"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodejs"
 
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
